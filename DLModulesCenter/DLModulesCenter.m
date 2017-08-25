bc4efa0c7b75a1bce4bb954b3b96f11adb2f16e1
lixu <lixu_sx@qiyi.com>
2017-08-25 15:34:19 +0800

    update shell file

---
+++ DLModulesCenter/DLModulesCenter.m
@@ -0,0 +1,63 @@
+//
+//  DLModulesCenter.m
+//  DLModulesCenter
+//
+//  Created by iqiyi on 17/8/25.
+//  Copyright © 2017年 iqiyi. All rights reserved.
+//
+
+#import "DLModulesCenter.h"
+
+@interface DLModulesManager ()
+
+@property (nonatomic ,strong) NSMutableDictionary *registerModules;
+
+@end
+
+@implementation DLModulesManager
+
++ (instancetype) sharedInstance
+{
+    static dispatch_once_t token;
+    static DLModulesManager *modulesManager = nil;
+    dispatch_once(&token, ^{
+        modulesManager = [[DLModulesManager alloc] init];
+        
+        modulesManager->_registerModules = [[NSMutableDictionary alloc] init];
+        
+    });
+    return modulesManager;
+}
+
++ (void) registerByModuleID:(NSString *)moduleID className:(NSString *)className
+{
+    DLModulesManager *modulesManager = [self sharedInstance];
+    
+    if (moduleID && className) {
+        [modulesManager.registerModules setObject:[moduleID description] forKey:[className description]];
+    }
+    
+}
+
++ (void) openModuleWithParams:(DLModuleParameter *)params
+{
+    NSString *moduleID = [params.originalParams objectForKey:DLModuleMainSeviceID];
+    
+    if (moduleID != nil) {
+        
+        DLModulesManager *modulesManager = [self sharedInstance];
+        
+        Class moduleClass;
+        NSString *className = [modulesManager.registerModules objectForKey:[moduleID description]];
+        moduleClass = NSClassFromString([className description]);
+        
+        id<DLModuleProtocol> module = (id<DLModuleProtocol>)moduleClass;
+        
+        if ([module respondsToSelector:@selector(launchWithParam:)]) {
+            [module launchWithParam:params];
+        }
+        
+    }
+}
+
+@end
